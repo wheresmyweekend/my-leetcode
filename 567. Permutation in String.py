26210579edc7953dbb26249bdfe8b618eed06fd6
Halim <halim261@gmail.com>
2022-10-08 11:54:49 +0800

    answer revision for qn567

--- 567. Permutation in String.py
+++ 567. Permutation in String.py
@@ -1,31 +1,46 @@
 
 def checkInclusion(s1: str, s2: str) -> bool:
+    # Impossible to have permutation if s1 > s2
     if len(s1) > len(s2): return False
 
+    # Create Hashmaps to quantify count
     s1Map = {}
     s2Map = {}
 
-    for c in s1:
-        s1Map[c] = 1 + s1Map.get(c, 0)
+    # Create hashmap for s1, while creating first sliding window in s2, based on length of s1
+    for i in range(len(s1)):
+        s1Map[s1[i]] = 1 + s1Map.get(s1[i], 0)
+        s2Map[s2[i]] = 1 + s2Map.get(s2[i], 0)
     
-    l,r,c = 0,len(s1) - 1, 0
+    # If first window is a hit, return True
+    if s1Map == s2Map:
+        return True
+    
+    # Create pointers 
+    # right pointer is set to 1 position after first window for easier coding
+    l, r = 0, len(s1)
 
-    while r < len(s2):
-        c = l
-        while l < r + 1:
-            s2Map[s2[l]] = 1 + s2Map.get(s2[l], 0)
-            l += 1
-        if s2Map == s1Map:
+    # Sliding window
+    # r-pointer adds values, l-pointer removes values
+    for r in range(len(s1), len(s2)):
+        # Adds new char from new position of r-pointer
+        s2Map[s2[r]] = 1 + s2Map.get(s2[r], 0)
+
+        # Removes char on l-pointer position before shifting 1 position to right
+        s2Map[s2[l]] -= 1
+        l += 1
+        # l-pointer removes empty keys
+        if s2Map[s2[l]] == 0:
+            del s2Map[s2[l]]
+
+        # Checks if Hashmaps match
+        if s1Map == s2Map:
             return True
-        else:
-            l = c 
-            l += 1
-            r += 1
-            s2Map = {}
     return False
 
+# Testcase
 if __name__ == "__main__":
-    u = "ab"
-    s = "eidbaooo"
+    u = "adc"
+    s = "dcda"
     answer = checkInclusion(u,s)
     print(answer)
