41b79984fb0cd57a26544e1be0dfc8e495192cc1
Halim <halim261@gmail.com>
2022-10-09 12:46:28 +0800

    added answer for qn20

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,13 +1,30 @@
 class Solution:
-    def isValid(self, s: str) -> bool:
-        
+    def isValid(s: str) -> bool:
+        # 1a. Create hashmap to match open and closing brackets
+        brackets = {")":"(", "}":"{", "]":"["}
+        # 1b. Create stack to hold open brackets
+        stack = []
 
-
-
+        for char in s:
+            # 2. If its an open bracket, add to stack
+            if char not in brackets.keys():
+                stack.append(char)
+            else:
+                # 3. If its a closing bracket, 
+                # 3a. check if stack empty and 
+                # 3b. check if last-added open bracket matches
+                if stack != [] and brackets[char] in stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+        # 4. if all pairs are cleared, return true
+        if stack == []:
+            return True
+        else:
+            return False
+            
 
 # Testcase
 if __name__ == "__main__":
-    u = "ab"
-    s = "eidbaooo"
-    answer = checkInclusion(u,s)
-    print(answer)
+    s = "}])"
+    print(Solution.isValid(s))
